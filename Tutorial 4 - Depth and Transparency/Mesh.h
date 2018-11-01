e367876ec31edb6b79ad71b9bd5b5f6eae6d5166
Marco Wang (Student) <b8011457@newcastle.ac.uk>
2018-11-01 14:30:51 +0000

    Tutorial 4 - Depth and Transparency

---
+++ Tutorial 4 - Depth and Transparency/Mesh.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "OGLRenderer.h"
+
+enum MeshBuffer {
+	VERTEX_BUFFER, COLOUR_BUFFER, TEXTURE_BUFFER, MAX_BUFFER
+};
+class Mesh
+{
+public:
+	Mesh(void);
+	~Mesh(void);
+
+	virtual void Draw();
+	static Mesh* GenerateTriangle();
+
+	void SetTexture(GLuint tex) { texture = tex;  }
+	GLuint GetTexture() { return texture;  }
+
+	static Mesh* GenerateQuad();
+
+protected:
+	void BufferData();
+
+	GLuint arrayObject;
+	GLuint bufferObject[MAX_BUFFER];
+	GLuint numVertices;
+	GLuint type;
+	GLuint texture;
+
+	Vector2* textureCoords;
+	Vector3* vertices;
+	Vector4* colours;
+};
+
+
