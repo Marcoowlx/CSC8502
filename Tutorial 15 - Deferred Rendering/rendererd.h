519b8e49f4fcb61b04bdecc4a45f0a9c46c1ee58
Marco Wang (Student) <b8011457@newcastle.ac.uk>
2018-11-13 18:10:29 +0000

    Tutorial 15 - Deferred Rendering

---
+++ Tutorial 15 - Deferred Rendering/rendererd.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "../../nclgl/OGLRenderer.h"
+#include "../../nclgl/Camera.h"
+#include "../../nclgl/OBJMesh.h"
+#include "../../nclgl/HeightMap.h"
+
+#define LIGHTNUM 8
+
+class Renderer : public OGLRenderer
+{
+public:
+	Renderer(Window &parent);
+	virtual ~Renderer(void);
+
+	virtual void RenderScene();
+	virtual void UpdateScene(float msec);
+
+protected:
+	void FillBuffers();
+	void DrawPointLights();
+	void CombineBuffers();
+	void GenerateScreenTexture(GLuint &into, bool depth = false);
+
+	Shader* sceneShader;
+	Shader* pointlightShader;
+	Shader* combineShader;
+
+	Light* pointLights;
+	Mesh* heightMap;
+	OBJMesh* sphere;
+	Mesh* quad;
+	Camera* camera;
+
+	float rotation;
+
+	GLuint bufferFBO;
+	GLuint bufferColourTex;
+	GLuint bufferNormalTex;
+	GLuint bufferDepthTex;
+
+	GLuint pointLightFBO;
+	GLuint lightEmissiveTex;
+	GLuint lightSpecularTex;
+};
+
